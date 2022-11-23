6ed08ce5727eb1b91e8a3e79fd6633372a042c12
amadozuniga11 <112574032+amadozuniga11@users.noreply.github.com>
2022-11-23 23:35:38 +0000

    made changes to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,5 +14,5 @@ def classes():
 def professors():
     return render_template('professors.html')
 
-'''if __name__ = '__main__': 
+'''if __name__ == '__main__': 
     app.run(debug=True)'''
