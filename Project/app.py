275a76f6d680c989fd0d6a360fe8b4a01a203c00
amadozuniga11 <112574032+amadozuniga11@users.noreply.github.com>
2022-11-23 23:18:31 +0000

    worked on website

---
+++ Project/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/classes')
+def classes(): 
+    return render_template('classes.html')
+
+@app.route('/professors')
+def professors():
+    return render_template('professors.html')
+
+'''if __name__ = '__main__': 
+    app.run(debug=True)'''
